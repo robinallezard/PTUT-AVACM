d031bb2fbb6c6b080935e640a236100c71a983ff
Robin Morel <robinallezard@gmail.com>
2018-02-06 14:38:37 +0100

    1ere VERSION HOMEPAGE / CONNEXION / SELECTION
    la slection.html n'est pas terminé

---
+++ connexion/connexion.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- feuilles de style -->
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" href="css/connexion.css">
+    <title>Connexion</title>
+</head>
+
+<body class="valign-wrapper">
+    <div id="login">
+        <img src="css/images/logo.svg" class="responsive-img" alt="logo AVACM">
+        <form action="" method="post">
+            <div class="input-field">
+                <input id="identifiant" type="text" class="validate">
+                <label for="identifiant">Votre login...</label>
+            </div>
+            <div class="input-field">
+                <input id="mdp" type="password" class="validate">
+                <label for="mdp">votre mot de passe...</label>
+            </div>
+             <button class="btn" type="submit" name="action">connexion
+  </button>
+        </form>
+    </div>
+    
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript">
+    
+    
+    </script>
+</body>
+
+</html>
